af458f063dc8907f0d228e54a126834bb33d8931
jorge aleman <jorge@subbo.es>
2020-04-30 22:46:54 +0200

    Google login working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin, {GoogleLoginResponse} from "react-google-login";
+
+function setUser(googleAccessResponse: GoogleLoginResponse) {
+  const user = {}
+  user.name = googleAccessResponse.getBasicProfile().getName()
+  user.accessToken = googleAccessResponse.accessToken
+  console.log("User logged in: " + user.name)
+}
 
 function App() {
   return (
@@ -18,6 +26,14 @@ function App() {
         >
           Learn React
         </a>
+        <GoogleLogin
+            clientId="994722567898-371a34efbppuihdd2uf13nmf4q6l1o11.apps.googleusercontent.com"
+            buttonText="Login with google"
+            onSuccess={setUser}
+            onFailure={console.log}
+            isSignedIn={true}
+            cookiePolicy={'single_host_origin'}
+        />
       </header>
     </div>
   );
